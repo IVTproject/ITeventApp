7331a9e3b65da13ab5c11a39b7c1b99d17ac9cff
Dmitry <sazonoff.dmitry2014@yandex.ru>
2016-03-08 18:09:10 +0300

    refresh button

---
+++ www/js/notification.js
@@ -0,0 +1,20 @@
+function send_notification() {
+    /*cordova.plugins.notification.local.schedule({
+		id: 1,
+		title: "Production Jour fixe",
+		text: "Duration 1h"
+		
+		firstAt: monday_9_am,
+		every: "week",
+		sound: "file://sounds/reminder.mp3",
+		icon: "http://icons.com/?cal_id=1",
+		data: { meetingId:"123#fg8" }
+		
+	});*/
+	
+	/*
+	cordova.plugins.notification.local.on("click", function (notification) {
+		joinMeeting(notification.data.meetingId);
+	});
+	*/
+}
---
+++ www/js/refresh_script.js
@@ -0,0 +1,25 @@
+function refresh_actions() {
+    load_schedule();
+}
+
+function refresh_events() {
+    list_all_events();
+}
+
+function refresh_informal() {
+    load_informal(0, 15);
+}
+
+function delete_refresh_button() {
+    $('#refresh_button').text("");
+}
+
+function change_refresh_button(name_refresh_button) {
+    $('#refresh_button').text("");
+    if(name_refresh_button == "event_page") {
+        $('#refresh_button').append('<img src="img/refresh.png" class="settings" onclick="refresh_actions();  refresh_informal();">');
+    }
+    if(name_refresh_button == "event_list") {
+        $('#refresh_button').append('<img src="img/refresh.png" class="settings" onclick="refresh_events();">');
+    }
+}
