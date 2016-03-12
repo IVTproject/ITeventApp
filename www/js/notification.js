5a0f3e0b594df5ffbd668376cb6cde9f661213f8
ilnur <ilnur11922@gmail.com>
2016-03-12 10:21:48 +0300

    notification manager

--- www/js/notification.js
+++ www/js/notification.js
@@ -1,20 +1,30 @@
-function send_notification() {
-    /*cordova.plugins.notification.local.schedule({
-		id: 1,
-		title: "Production Jour fixe",
-		text: "Duration 1h"
-		
-		firstAt: monday_9_am,
-		every: "week",
-		sound: "file://sounds/reminder.mp3",
-		icon: "http://icons.com/?cal_id=1",
-		data: { meetingId:"123#fg8" }
-		
-	});*/
+function send_notification(notif_item) {
 	
-	/*
-	cordova.plugins.notification.local.on("click", function (notification) {
-		joinMeeting(notification.data.meetingId);
+	var time = new Date(notif_item.time);
+	
+    cordova.plugins.notification.local.schedule({
+		id: notif_item.id,
+		title: notif_item.title,
+		text: notif_item.text,
+		at: time,
+		icon: "http://it-event.tk/img/icon.png"
+		//data: { meetingId:"123#fg8" }
 	});
-	*/
+	
 }
+
+function cancel_notification(id) {
+	cordova.plugins.notification.local.clear(id);
+	cordova.plugins.notification.local.cancel(id);
+}
+
+cordova.plugins.notification.local.on("click", function (notification) {
+	alert("dsfgb");
+});
+
+function ok() {
+	notif_item = ' { "id" : "1", "title" : "Hello", "text" : "Hello world", "time" : "2016-03-11 15:35:00" } ';
+	notif_item = JSON.parse(notif_item);
+	send_notification(notif_item);
+}
+
